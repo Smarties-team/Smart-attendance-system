26ad84837d901dadae5dcb23a1670ea4fbf70856
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2022-04-18 11:00:54 +0200

    Add error handling

--- public/recognizeStudent.py
+++ public/recognizeStudent.py
@@ -18,10 +18,14 @@ import mysql.connector as msql
 studentID = sys.argv[1]
 
 # Validate the input argument
+try:
+    int(studentID)
+except Exception as e:
+    print("Invalid studentID: ", studentID, ", due to: ", e)
+    exit()
 
-print("Student ID:", studentID)
 
-# Access database
+# Connect to database
 connection_config_dict = {
     "user": "root",
     "password": "diaa",
@@ -34,81 +38,78 @@ connection_config_dict = {
     "pool_size": 5,  # connection pool size
 }
 
+try:
+    connection = msql.connect(**connection_config_dict)
 
-connection = msql.connect(**connection_config_dict)
+    if connection.is_connected() != True:
+        print("Failed to connect to database")
+        exit()
 
-if connection.is_connected():
-    db_Info = connection.get_server_info()
-    print("Connected to MySQL Server version ", db_Info)
+except Exception as e:
+    print("Failed to connect due to ", e)
+    exit()
 
-
-cursor = connection.cursor()
-
-
-# Get photo path from database
-cursor.execute("SELECT photo FROM students WHERE id=" + studentID)
-result = cursor.fetchall()
-print("Fetched: ", result)
-path = str(result[0][0])
-fullPath = "../storage/app/" + path
-print("full path", fullPath)
-
-cursor.close()
-connection.close()
-
-
-# Open image
-image = face_recognition.load_image_file(fullPath)
-
-# Vaildate a human face
-face_locations = face_recognition.face_locations(image, model="cnn")
-
-if len(face_locations) < 1:
-    print("Invalid photo, no face detected")
-    exit
-elif len(face_locations) > 1:
-    print("Invalid photo, multiple faces detected")
-    exit
-
-
-# Store encodings in database
-# Align the image first
-# top, right, bottom, left = face_locations(0)
-# desiredWidth = (right-left)
-# desiredHeight = (bottom-top)
-# face_landmarks = face_recognition.face_landmarks(image)
-# align_f = alignFace(image, face_locations, face_landmarks, desiredWidth, desiredHeight)
-face_encodings = face_recognition.face_encodings(image, num_jitters=10)[0]
-
-serializedEncodings = pickle.dumps(face_encodings)
-
-connection = msql.connect(**connection_config_dict)
-
-if connection.is_connected():
-    db_Info = connection.get_server_info()
-    print("Connected to MySQL Server version ", db_Info)
-
-
-cursor = connection.cursor()
-
-
-sqlStmt = "UPDATE students SET face_encoding = %s WHERE id= %s"
-
-f = open("myfile", "rb")
-vals = (serializedEncodings, studentID)
 
 try:
-    cursor.execute(sqlStmt, vals)
+    cursor = connection.cursor()
+
+    # Get photo path from database
+    cursor.execute("SELECT photo FROM students WHERE id=" + studentID)
+    result = cursor.fetchall()
+    print("Fetched: ", result)
+    path = str(result[0][0])
+    fullPath = "../storage/app/" + path
+    print("full path", fullPath)
+
+    # Open image
+    image = face_recognition.load_image_file(fullPath)
+
+    # Vaildate a human face
+    face_locations = face_recognition.face_locations(image, model="cnn")
+    if len(face_locations) < 1:
+        # print("Invalid photo, no face detected")
+        raise Exception("Invalid photo, no face detected")
+    elif len(face_locations) > 1:
+        raise Exception("Invalid photo, multiple faces detected")
+
+    # Store encodings in database
+    # Align the image first
+    # top, right, bottom, left = face_locations(0)
+    # desiredWidth = (right-left)
+    # desiredHeight = (bottom-top)
+    # face_landmarks = face_recognition.face_landmarks(image)
+    # align_f = alignFace(image, face_locations, face_landmarks, desiredWidth, desiredHeight)
+    face_encodings = face_recognition.face_encodings(image, num_jitters=10)[0]
+
+    serializedEncodings = pickle.dumps(face_encodings)
+
+    sqlStmt = "UPDATE students SET face_encoding = %s WHERE id= %s"
+    vals = (serializedEncodings, studentID)
+
+    # Perform and validate the SQL update command
+    try:
+        cursor.execute(sqlStmt, vals)
+    except Exception as e:
+        if str(e) == "1300: Invalid utf8mb4 character string: '800363'":
+            # Ignore this error
+            []
+        else:
+            raise Exception("Failed to execute the face encodings insertion due to ", e)
+            # exit()
+
+    connection.commit()
+
+    # Validate the affected rows count
+    result = cursor.rowcount
+    if result == 1:
+        print("Success")
+    else:
+        raise Exception("Rows affected should be 1, but got: ", result)
+
 except Exception as e:
-    print("excep")
-    print(e)
+    print("Failure, ", e)
 
-
-connection.commit()
-result = cursor.rowcount
-print("Rows affected: ", result)
-
-
-cursor.close()
-connection.close()
+finally:
+    cursor.close()
+    connection.close()
 
