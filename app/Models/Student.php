7c80f90b7642c06f691b952140621cdd0138db84
Diaa Eldeen Esmail Ahmed <diaaeldean56@gmail.com>
2022-04-26 22:05:54 +0200

    Add model for students table

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+
+    public function logs()
+    {
+        $this->hasMany(Students_log::class);
+    }
 }
